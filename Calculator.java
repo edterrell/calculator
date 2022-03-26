37f14ddee02d4e70da44d625a7a1a0b625d7548c
Ed Terrell <edterrell@comcast.net>
2022-03-26 15:46:46 -0600

    "Adding a method to Calculator.java class"

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,9 @@
 public class Calculator {}
-public class Calculator() {
+public class Calculator {
     public static void main(String[] args) {}
+}
+public class Calculator {
+  public int addition(int a, int b) {
+    return a + b;
+  }
 }
